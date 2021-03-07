7c4ec66b2185cdccc157986d9ba65aa4a9195e4d
kino-ma <kino.ma.ms@gmail.com>
2021-03-07 11:03:13 +0900

    [feat] enable hot reloading for elm

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,37 @@
-var elmSoruce = __dirname
+const path = require('path');
+const webpack = require('webpack');
+
+var elmSoruce = __dirname;
 
 module.exports = {
+    devServer: {
+        contentBase: [
+            path.join(__dirname, './src'),
+            path.join(__dirname, './dist'),
+        ],
+        hot: true,
+        host: "0.0.0.0",
+        port: 3001,
+        inline: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         rules: [{
             test: /\.elm$/,
             exclude: [/elm-stuff/, /node_modules/],
-            use: {
-                loader: 'elm-webpack-loader',
-                options: {
-                    cwd: elmSoruce
+            use: [
+                { loader: 'elm-hot-webpack-loader' },
+                {
+                    loader: 'elm-webpack-loader',
+                    options: {
+                        cwd: elmSoruce,
+                        debug: false
+                    }
                 }
-            }
+            ]
         },
         {
             test: /\.html$/,
