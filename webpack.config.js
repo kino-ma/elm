794b95728939ffad607b81bd7733d38a8b042bac
kino-ma <kino.ma.ms@gmail.com>
2021-03-26 00:48:06 +0900

    [wip] Can't resolve './main.js' in '/app/src'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 var elmSoruce = __dirname;
 
@@ -16,8 +17,17 @@ module.exports = {
         historyApiFallback: true,
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+            favicon: "./src/static/favicon.ico",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "main.js",
+    },
     module: {
         rules: [{
             test: /\.elm$/,
@@ -33,29 +43,26 @@ module.exports = {
                 }
             ]
         },
-        {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'file-loader?name=[name].[ext]'
-            }
-            },
             {
-                test: /\.(css|scss)$/,
-                include: [/src/],
-                exclude: [/elm-stuff/, /node_modules/],
-                use: [
-                    'style-loader',
-                    {
-                        'loader': 'css-loader',
-                        'options': {
-                            'modules': 'pure',
-                            'modules': {
-                                'localIdentName': '[name]__[local]',
-                            }
-                        },
-                    }
-                ]
+                test: /\.html$/i,
+                loader: 'html-loader'
+        },
+            {
+            test: /\.(css|scss)$/,
+            include: [/src/],
+            exclude: [/elm-stuff/, /node_modules/],
+            use: [
+                'style-loader',
+                {
+                    'loader': 'css-loader',
+                    'options': {
+                        'modules': 'pure',
+                        'modules': {
+                            'localIdentName': '[name]__[local]',
+                        }
+                    },
+                }
+            ]
         }
         ]
     }
