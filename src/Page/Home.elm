83e319ebfa7eb73544310f6facdc2710b8a6cfed
kino-ma <kino.ma.ms@gmail.com>
2021-04-03 10:42:06 +0900

    [feat] general ul class

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -50,7 +50,7 @@ view model =
                         [ cardTextContent "慶応大学環境情報学部三年　牧野青希"
                         ]
                     , cardTile [] (text "Awards")
-                        [ ul [ class Style "card-content" ]
+                        [ ul [ class Style "card-content", class Style "general-list-items" ]
                             [ li [] [text "Hack U SFC 2019 最優秀賞"]
                             , li [] [text "Open Hack U Online 2020 vol.2 最優秀賞"]
                             , li [] [text "Hack Day Online 2021 Eaglys賞"]
